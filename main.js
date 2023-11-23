3ad6f0dea3e88c6d48f8ae9ed278832f40876a30
jocnunez <jose.nunnez@gmail.com>
2023-11-23 20:55:41 +0100

    javascript basics

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-//Practica 1
-let msg = "hola mundo"
+//Variables
+//var
+//ya no se usa. funciona igual que let
 
-console.log(msg)
+//nombre: palabra reservada, no empezar por número, ni simbolos como el -
+//Valores primitivos
+let nombre = 'Mi nombre' //string
+let edad = 30 //number
+let soltero = true //(o false) Boolean
+let zero = null //Valor especial conocido
+let incognita //undefined (a evitar)
 
-document.addEventListener("DOMContentLoaded", init)
-
-function init(event) {
-    let title = document.getElementById("title")
-    title.style.color = "red"
-    title.innerHTML = msg
-    loadData("data.json")
+//Valores referenciales
+//object - parejas clave/valor separadas por ,
+let ficha = {
+    nombre: 'Mi nombre',
+    edad: 30,
+    soltero: true
 }
 
-//No hace falta que entiendan esta funcion todavía
-function loadData(url) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => pintaMisHobbies(data))
-}
+//acceso a propiedades
+//por .
+console.log(ficha.nombre)
+//por nombre de propiedad
+console.log(ficha['nombre'])
 
-function pintaMisHobbies(json) {
-    console.log(json)
+//Array
+let lista = [
+    'paul',
+    'john',
+    'george',
+    'ringo'
+]
+console.log(lista)
+
+//acceso a elementos
+console.log(lista[3])
+
+//Funciones
+let imprimir = function algo(usuario) {
+    let nosvamosya = true
+    console.log('Hola ', usuario.nombre)
+    console.log('tu edad es: ', usuario.edad)
 }
+console.log(nosvamosya)
+imprimir(ficha)
+
+
+//diferencia entre valor y referencia
+let a = {
+    clave1: 30,
+    clave2: 50
+}
+let b = a
+a.clave1 = 40
+
+console.log(a)
+console.log(b)
+
 
 
